1f68e3547caba02131b803082af05b3a39a6cd73
walterlicinio <licinio.laticinio@gmail.com>
2021-05-26 13:16:59 -0300

    Card and User Models

---
+++ netcoreapi/netcoreapi/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreapi.Models
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ netcoreapi/netcoreapi/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoreapi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+    }
+}
